418920c2fbe20d21eadd0c88178392150e3422e8
Carlos Grossi <carlos.grossi@gmail.com>
2016-07-11 21:22:32 -0300

    Initial Commit

---
+++ YTKit/YTKit.h
@@ -0,0 +1,19 @@
+//
+//  YTKit.h
+//  YTKit
+//
+//  Created by Carlos Grossi on 11/7/16.
+//  Copyright © 2016 Carlos Grossi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for YTKit.
+FOUNDATION_EXPORT double YTKitVersionNumber;
+
+//! Project version string for YTKit.
+FOUNDATION_EXPORT const unsigned char YTKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YTKit/PublicHeader.h>
+
+
